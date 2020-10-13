44e83d2fcda15378d494bcf922ac7784c5571ad9
wenjiajia <herowenjj@gmail.com>
2020-10-13 23:26:58 +0800

    TODO：滑动窗口

--- maxSlidingWindow_test.go
+++ maxSlidingWindow_test.go
@@ -1,11 +1,46 @@
 package mleetcode
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 //239. 滑动窗口最大值
+type MyList []int
+
+func (m MyList) Len() int {
+	panic("implement me")
+}
+
+func (m MyList) Less(i, j int) bool {
+	// 维护最大堆
+	panic("implement me")
+}
+
+func (m MyList) Swap(i, j int) {
+	panic("implement me")
+}
 
 func maxSlidingWindow(nums []int, k int) []int {
+	var ret_list []int
 	//TODO：堆，优先队列（双端队列）
+	//取前k个元素,排序和维护堆，原来nums不改变，方便后续for循环遍历
+	new_nums := nums
+	sort.Sort(MyList(new_nums))
+	length := len(new_nums)
+	if len(new_nums) > k {
+		new_nums = new_nums[len(new_nums)-k:]
+	}
+
+	//heap.Init()
+	for i := 0; i < length; i++ {
+		if i < k {
+			ret_list = append(ret_list, new_nums[0])
+		} else {
+
+		}
+
+	}
 	return nums
 }
 
