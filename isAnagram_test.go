1b6acfa57e72af5fec786773ef97d04791d42e0b
wenjiajia <herowenjj@gmail.com>
2020-10-20 22:45:44 +0800

    有效的字母异位词,方法2

--- isAnagram_test.go
+++ isAnagram_test.go
@@ -20,9 +20,25 @@ func isAnagram(s string, t string) bool {
 	return true
 }
 
+func isAnagram1(s string, t string) bool {
+	a := [26]int{}
+	b := [26]int{}
+
+	for _, v := range s {
+		a[v-'a'] += 1
+	}
+
+	for _, v := range t {
+		b[v-'a'] += 1
+	}
+
+	return a == b
+}
+
 func TestIsAnagram(t *testing.T) {
 	strA := "cat"
 	strB := "tac"
 	isAnagram(strA, strB)
+	isAnagram1(strA, strB)
 
 }
