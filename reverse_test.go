d3cb7d0df04fa6a38f1e549aed185c75db675835
wenjiajia <herowenjj@gmail.com>
2021-06-10 22:21:08 +0800

    翻转链表2

--- reverse_test.go
+++ reverse_test.go
@@ -1,6 +1,7 @@
 package mleetcode
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -18,7 +19,7 @@ type ListNode struct {
 }
 
 //leetcode 206
-func reverseList(head *ListNode) *ListNode {
+func reverseList1(head *ListNode) *ListNode {
 	var res *ListNode
 	for head != nil {
 		temp := head.Next
@@ -47,5 +48,18 @@ func TestReverseNode(t *testing.T) {
 		temp.Next = &ListNode{i, nil}
 		temp = temp.Next
 	}
-	reverseList2(res.Next)
+	reverseList(res.Next)
+	//reverseList2(res.Next)
+}
+
+func reverseList(head *ListNode) {
+	var res *ListNode
+	//这题的精髓是构造了一个空的节点，每次将这个节点追加到当前的next，不断遍历成倒序
+	for head != nil {
+		a := head.Next
+		head.Next = res
+		res = head
+		head = a
+	}
+	fmt.Println(res)
 }
